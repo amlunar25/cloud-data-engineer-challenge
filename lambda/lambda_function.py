9424b70fda6816f134c70a982626f43b4c50fd46
Alexander Luna <alluna@fanatics.com>
2025-10-07 16:19:05 -0500

    Fixing lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -22,8 +22,6 @@ def handler(event, context):
             for record in event["Records"]:
                 bucket = record["s3"]["bucket"]["name"]
                 key = record["s3"]["object"]["key"]
-                bucket = "my-private-bucket-terraform-test-nanlab1"
-                key = "sales.csv"
 
                 response = s3_client.get_object(Bucket=bucket, Key=key)
                 file_content = response["Body"].read().decode("utf-8")
